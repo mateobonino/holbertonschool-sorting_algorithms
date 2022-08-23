a4991ce0d3c8593a5d3705db736f4f8e07ba5ecc
mateo <mateo@localhost.localdomain>
2022-08-23 17:29:35 -0400

    Task 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,44 @@
 #include "sort.h"
 /**
-*
-*
-*
-*
+* selection_sort - sorts an array of integers in ascending order
+* @array: the array
+* @size: the array size
+* Return: void function
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, temp = 0;
+	size_t i = 0, j = 0, min = 0, print = 0;
 
-    if (!array || !size)
-        return;
-    for (i = 0; array[i] && i < size - 1; i++)
-    {
-        if (array[i] > temp )
-        (
-            for (j = i + 1; array[j] && j < size - i - 1; j++)
-            {
-                if (array[i] > temp )
-                {
-                    temp = array[j];
-                    continue;
-                }
-            }
-        )
-    }
+	if (!array || !size)
+		return;
+	for (i = 0; array[i] && i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; array[j] && j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+				print = 1;
+			}
+		}
+		swap_arr(&array[i], &array[min]);
+		if (print)
+			print_array(array, size);
+		print = 0;
+	}
+}
+/**
+* swap_arr - swaps an array
+* @a: the first position
+* @b: the second position
+* Return: void function
+*/
+void swap_arr(int *a, int *b)
+{
+	unsigned int temp = 0;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
