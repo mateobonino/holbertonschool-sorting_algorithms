c4a67ef27aac6b4ee533e1ce177561a24f6c31a6
juan <fernan2lopezkto@gmail.com>
2022-08-24 08:45:40 -0700

    try any thinks

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,8 @@
 #include "sort.h"
+
+void swap_quicksort(int *a, int *b);
+int particion(int *array, int izquierda, int derecha);
+
 /**
 *
 *
@@ -6,11 +10,52 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, pivot = 0, aux = 0;
+	int indice_particion;
 
-	if (!array || !size)
-		return;
-	i = size;
-	
-	
+	if(array[0] < (int)size - 1)
+		indice_particion = particion(array, array[0], size);
+
+	print_array((const int *)array, size);
+	quick_sort(array, indice_particion);
+	quick_sort(&array[indice_particion], size - 1);
+}
+
+
+
+
+
+void swap_quicksort(int *a, int *b)
+{
+	int temporal = *a;
+	*a = *b;
+	*b = temporal;
+}
+
+
+
+int particion(int *array, int izquierda, int derecha)
+{
+	int pivote = array[izquierda];
+
+	while (1)
+	{
+		while(array[izquierda] < pivote)
+		{
+			izquierda++;
+		}
+		while(array[derecha] > pivote)
+		{
+			derecha--;
+		}
+		if (izquierda >= derecha)
+		{
+			return (derecha);
+		}
+		else
+		{
+			swap_quicksort(&array[izquierda], &array[derecha]);
+			izquierda++;
+			derecha--;
+		}
+	}
 }
